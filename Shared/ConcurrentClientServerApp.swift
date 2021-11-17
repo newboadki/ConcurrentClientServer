74d6187bf499c68101aefb368e0c1b3cf8e9ee3f
Borja Arias Drake <borja.arias@gmail.com>
2021-11-17 05:25:43 +0100

    Initial Commit

---
+++ Shared/ConcurrentClientServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrentClientServerApp.swift
+//  Shared
+//
+//  Created by Borja Arias Drake on 17.11.2021..
+//
+
+import SwiftUI
+
+@main
+struct ConcurrentClientServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
